5796a9f52943e0505dc123c68a9fc66f7790c2ee
Augusto Cesar <acs-03@hotmail.com>
2021-04-29 17:26:23 -0300

    exercise DOM

---
+++ acs.js
@@ -0,0 +1,13 @@
+function calculateAverage(){
+
+    var total = 0;
+    var amount = arguments.length;
+    var x = 0;
+
+    while(typeof arguments[x] === "number"){
+        total += arguments[x];
+        x++;
+    }
+
+    return total / amount;
+}
